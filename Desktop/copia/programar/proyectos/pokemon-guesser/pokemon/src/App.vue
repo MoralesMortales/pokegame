bc997c9352cc61daf130dc1404ed7e1a2717c743
ssuf <ssuf2002@gmail.com>
2024-05-04 11:25:34 -0400

    commit para carlos con add

---
+++ Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Pokemon } from 'env';
+  import getPokemonOptions from './api/getPokemonOptions';
+  import { ref } from 'vue';
+  
+
+  const pokemonArr = ref<Pokemon[]>([])
+  const pokemon = ref<Pokemon>()
+
+  const mixPokemonArray = async()=>{
+    pokemonArr.value = await getPokemonOptions()
+    const randomInt = Math.floor(Math.random() * 4)
+    pokemon.value = pokemonArr.value[randomInt]
+    console.log(pokemon.value)
+    console.log(pokemonArr.value)
+  }
+
+  mixPokemonArray()
+</script>
+
+<template>
+ <p class="text-9xl">hola tonotos</p>
+</template>
+
---
+++ Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/src/api/getPokemonOptions.ts
@@ -0,0 +1,36 @@
+import { pokemonApi } from "./pokemonApi";
+import type { Pokemon } from "env";
+
+const getPokemons = (): number[] =>{
+    const pokemonArr = Array.from(Array(650))
+    return pokemonArr.map((_, index) => index + 1)
+}
+const pokemonName = async(pokemons : number[]): Promise<Pokemon[]> =>{
+    if(pokemons.length != 4) throw "Error pinche pendejo"
+    const [a,b,c,d]= pokemons
+
+    const promiseArr =[
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`)
+    ]
+
+    const [{data:p1}, {data:p2}, {data:p3}, {data:p4},] = await Promise.all(promiseArr)
+
+    return [
+
+        {name: p1.name, id:p1.id},
+        {name: p2.name, id:p2.id},
+        {name: p3.name, id:p3.id},
+        {name: p4.name, id:p4.id}
+    ]
+}
+
+const getPokemonOptions =async () => {
+    const mixedPokemons = getPokemons().sort(()=>Math.random() -0.5)
+    console.log(mixedPokemons)
+    const pokemons = await pokemonName(mixedPokemons.splice(0,4))
+}
+
+export default getPokemonOptions
