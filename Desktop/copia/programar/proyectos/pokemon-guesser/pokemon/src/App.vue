bae015a9926aa2842c0eaace46e4430a475b3c12
ssuf <ssuf2002@gmail.com>
2024-05-11 12:04:43 -0400

    commit imagen responsive

--- Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/src/App.vue
+++ Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/src/App.vue
@@ -8,6 +8,9 @@
 
   const pokemonArr = ref<any>([])
   const pokemon = ref<Pokemon>()
+  const showPokemon = ref<boolean>(false)
+  const showAnswers = ref<boolean>(false)
+  const message = ref<string>()
 
   const mixPokemonArray = async()=>{
     pokemonArr.value = await getPokemonOptions()
@@ -16,17 +19,32 @@
     
   }
 
+  const checkAnswer = (selectedId: number)=>{
+    showPokemon.value = true
+    showAnswers.value = true
+
+    if(selectedId === pokemon.value?.id){
+      message.value = `Correcto, era un ${pokemon.value.name}`
+    }
+    else{
+      message.value = `Mongolo, eso es un ${pokemon.value?.name}`
+    }
+  }
+
   mixPokemonArray()
 </script>
 
 <template>
   <div class="m-12">
-    <PokemonPicture v-if="pokemon" :pokemon-id="pokemon.id" />
-    <PokemonOptions :pokemons="pokemonArr" /> 
+    <PokemonPicture :show-pokemon="showPokemon" v-if="pokemon" :pokemon-id="pokemon.id" />
+    <PokemonOptions @selection-pokemon="checkAnswer" :pokemons="pokemonArr" /> 
   </div>
 
   <!-- <div class="flex justify-center mt-14" v-for=""pokemon in pokemons
   <p class="text-9xl">hola tonotos</p> -->
+    <div class="text-center">
+      <p class="text-2xl"> {{ message }}</p>
+    </div>
 
 </template>
 
--- Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/src/components/PokemonPicture.vue
+++ Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/src/components/PokemonPicture.vue
@@ -1,15 +1,24 @@
 <template>
-    <div class="flex justify-center">
-        <img :src="img" alt="imagen-pokemon" />
+    <div  class="flex justify-center">
+        <img v-if="!showPokemon" class="hidden-pokemon":src="img" alt="imagen-pokemon" />
+        <img v-else :src="img" alt="imagen-pokemon" />
     </div>
 </template>
 
 <script lang="ts" setup>
     const props = defineProps<{
-        pokemonId: number
+        pokemonId: number,
+        showPokemon: boolean
     }>()
 
     const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ props.pokemonId }.svg`;
 
 
 </script>
+
+<style>
+    .hidden-pokemon{
+        filter: brightness(0);
+    }
+
+</style>
