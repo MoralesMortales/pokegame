fd7e14f141e69f01e7aafab0af49800189894969
CarlosLup <moralesyaguilera@gmail.com>
2024-05-25 11:14:39 -0400

    before pull

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  
+  base:'/pokemon/', //pokemon es la ruta del proyecto
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
